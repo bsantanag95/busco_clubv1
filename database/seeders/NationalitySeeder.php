72a1adc003a918452e89fc3b4ebadeab81dd6c7d
besantana95 <besantanag95@gmail.com>
2024-11-07 20:52:02 -0300

    Segunda actualizacion de nacionalidades

--- database/seeders/NationalitySeeder.php
+++ database/seeders/NationalitySeeder.php
@@ -78,6 +78,10 @@ class NationalitySeeder extends Seeder
             'flag' => 'flags/bulgaria.jpg'
         ]);
         Nationality::create([
+            'country' => 'Cabo Verde',
+            'flag' => 'flags/cabo_verde.jpg'
+        ]);
+        Nationality::create([
             'country' => 'Camerún',
             'flag' => 'flags/camerun.jpg'
         ]);
@@ -110,6 +114,10 @@ class NationalitySeeder extends Seeder
             'flag' => 'flags/colombia.jpg'
         ]);
         Nationality::create([
+            'country' => 'Corea del Sur',
+            'flag' => 'flags/corea.jpg'
+        ]);
+        Nationality::create([
             'country' => 'Costa de Marfil',
             'flag' => 'flags/costa_de_marfil.jpg'
         ]);
@@ -138,12 +146,272 @@ class NationalitySeeder extends Seeder
             'flag' => 'flags/ecuador.jpg'
         ]);
         Nationality::create([
+            'country' => 'Egipto',
+            'flag' => 'flags/egipto.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'El Salvador',
+            'flag' => 'flags/el_salvador.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Emiratos Árabes Unidos',
+            'flag' => 'flags/eau.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Escocia',
+            'flag' => 'flags/escocia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Eslovaquia',
+            'flag' => 'flags/eslovaquia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Eslovenia',
+            'flag' => 'flags/eslovenia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'España',
+            'flag' => 'flags/españa.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Estados Unidos',
+            'flag' => 'flags/usa.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Estonia',
+            'flag' => 'flags/estonia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Filipinas',
+            'flag' => 'flags/filipinas.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Finlandia',
+            'flag' => 'flags/finlandia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Francia',
+            'flag' => 'flags/francia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Gales',
+            'flag' => 'flags/gales.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Gambia',
+            'flag' => 'flags/gambia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Georgia',
+            'flag' => 'flags/georgia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Ghana',
+            'flag' => 'flags/ghana.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Gibraltar',
+            'flag' => 'flags/gibraltar.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Grecia',
+            'flag' => 'flags/grecia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Guam',
+            'flag' => 'flags/guam.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Guatemala',
+            'flag' => 'flags/guatemala.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Guinea',
+            'flag' => 'flags/guinea.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Guinea Ecuatorial',
+            'flag' => 'flags/guinea_ecuatorial.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Haiti',
+            'flag' => 'flags/haiti.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Honduras',
+            'flag' => 'flags/honduras.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Hong Kong',
+            'flag' => 'flags/hong_kong.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Hungría',
+            'flag' => 'flags/hungria.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'India',
+            'flag' => 'flags/india.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Indonesia',
+            'flag' => 'flags/indonesia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Inglaterra',
+            'flag' => 'flags/inglaterra.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Irak',
+            'flag' => 'flags/irak.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Irán',
+            'flag' => 'flags/iran.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Irlanda del Norte',
+            'flag' => 'flags/irlanda_del_norte.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Islandia',
+            'flag' => 'flags/islandia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Islas Feroe',
+            'flag' => 'flags/islas_feroe.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Israel',
+            'flag' => 'flags/israel.jpg'
+        ]);
+        Nationality::create([
             'country' => 'Italia',
             'flag' => 'flags/italy.jpg'
         ]);
         Nationality::create([
+            'country' => 'Jamaica',
+            'flag' => 'flags/jamaica.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Japón',
+            'flag' => 'flags/japon.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Jordania',
+            'flag' => 'flags/jordania.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Kazajistán',
+            'flag' => 'flags/kazajistan.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Kenia',
+            'flag' => 'flags/kenia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Kosovo',
+            'flag' => 'flags/kosovo.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Kuwait',
+            'flag' => 'flags/kuwait.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Laos',
+            'flag' => 'flags/laos.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Letonia',
+            'flag' => 'flags/letonia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Libano',
+            'flag' => 'flags/libano.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Libia',
+            'flag' => 'flags/libia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Liechtenstein',
+            'flag' => 'flags/liechtenstein.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Lituania',
+            'flag' => 'flags/lituania.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Luxemburgo',
+            'flag' => 'flags/luxemburgo.jpg'
+        ]);
+        Nationality::create([
             'country' => 'Macedonia del Norte',
             'flag' => 'flags/macedonia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Malasia',
+            'flag' => 'flags/malasia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Mali',
+            'flag' => 'flags/mali.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Malta',
+            'flag' => 'flags/malta.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Marruecos',
+            'flag' => 'flags/malta.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'México',
+            'flag' => 'flags/mexico.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Moldavia',
+            'flag' => 'flags/moldavia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Montenegro',
+            'flag' => 'flags/montenegro.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Nicaragua',
+            'flag' => 'flags/nicaragua.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Nigeria',
+            'flag' => 'flags/nigeria.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Noruega',
+            'flag' => 'flags/noruega.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Nueva Zelanda',
+            'flag' => 'flags/nueva_zelanda.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Omán',
+            'flag' => 'flags/oman.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Países Bajos',
+            'flag' => 'flags/paises_bajos.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Pakistán',
+            'flag' => 'flags/pakistan.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Palestina',
+            'flag' => 'flags/palestina.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Panamá',
+            'flag' => 'flags/panama.jpg'
         ]);
         Nationality::create([
             'country' => 'Paraguay',
@@ -154,12 +422,140 @@ class NationalitySeeder extends Seeder
             'flag' => 'flags/peru.jpg'
         ]);
         Nationality::create([
+            'country' => 'Polonia',
+            'flag' => 'flags/polonia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Portugal',
+            'flag' => 'flags/portugal.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Puerto Rico',
+            'flag' => 'flags/puerto_rico.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'RD del Congo',
+            'flag' => 'flags/rd_congo.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'República Checa',
+            'flag' => 'flags/republica_checa.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'República de Irlanda',
+            'flag' => 'flags/irlanda.jpg'
+        ]);
+        Nationality::create([
             'country' => 'República del Congo',
             'flag' => 'flags/congo.jpg'
         ]);
         Nationality::create([
+            'country' => 'República Dominicana',
+            'flag' => 'flags/republica_dominicana.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Rumania',
+            'flag' => 'flags/rumania.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Rusia',
+            'flag' => 'flags/rusia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'San Marino',
+            'flag' => 'flags/san_marino.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Senegal',
+            'flag' => 'flags/senegal.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Serbia',
+            'flag' => 'flags/serbia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Singapur',
+            'flag' => 'flags/singapur.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Siria',
+            'flag' => 'flags/siria.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Sudáfrica',
+            'flag' => 'flags/sudafrica.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Sudán',
+            'flag' => 'flags/sudan.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Suecia',
+            'flag' => 'flags/suecia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Suiza',
+            'flag' => 'flags/suiza.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Surinam ',
+            'flag' => 'flags/surinam.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Tahiti ',
+            'flag' => 'flags/tahiti.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Tailandia ',
+            'flag' => 'flags/tailandia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Togo ',
+            'flag' => 'flags/togo.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Trinidad y Tobago',
+            'flag' => 'flags/tyt.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Túnez ',
+            'flag' => 'flags/tunez.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Turquía',
+            'flag' => 'flags/turquia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Ucrania',
+            'flag' => 'flags/ucrania.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Uganda',
+            'flag' => 'flags/uganda.jpg'
+        ]);
+        Nationality::create([
             'country' => 'Uruguay',
             'flag' => 'flags/uruguay.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Uzbekistán',
+            'flag' => 'flags/uzbekistan.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Venezuela',
+            'flag' => 'flags/venezuela.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Vietnam',
+            'flag' => 'flags/vietnam.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Zambia',
+            'flag' => 'flags/zambia.jpg'
+        ]);
+        Nationality::create([
+            'country' => 'Zimbabue',
+            'flag' => 'flags/zimbabue.jpg'
         ]);
     }
 }
